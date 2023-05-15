132b1be773ca9e9e61e8d861b97fbc7d1cf49cbd
Joseph Kraft <joseph.kraft@alula.net>
2023-05-15 16:38:13 -0500

    Tutorial up to express installation

--- index.js
+++ index.js
@@ -12,3 +12,16 @@ eventEmitter.on('lunch', () => {
 })
 eventEmitter.emit('lunch');
 eventEmitter.emit('lunch');
+
+const { readFile, readFileSync } = require('fs');
+const txt = readFileSync('./hello.txt', 'utf8');
+console.log(txt);
+console.log('DO THIS ASAP 1'); //This is done after
+
+readFile('./hello.txt', 'utf8', (err,txt) => {
+    console.log(txt)
+});
+console.log('DO THIS ASAP 2'); //This is done before
+
+const myModule = require('./my-module.js');
+console.log(myModule);
